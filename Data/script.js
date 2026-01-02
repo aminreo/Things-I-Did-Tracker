d5f3be45c95a802caeb22b26f87c4ab3b185057e
Amin <ayari.am@campus.lmu.de>
2026-01-02 17:18:20 +0100

    working initial demo

---
+++ Data/script.js
@@ -0,0 +1,72 @@
+
+
+
+let typed_content = '';
+let chosen_difficulty = '*';
+let current_date = new Date().toLocaleDateString();
+
+// localStorage.setItem('savedNotes', JSON.stringify([{ content: "test", difficulty: chosen_difficulty, date: current_date }]))
+let savedNotes = [];
+const saved = localStorage.getItem('savedNotes');
+if (saved){
+    savedNotes= JSON.parse(saved);
+}
+
+
+const textInputElem = document.getElementById('text-input');
+textInputElem.addEventListener('input', handleInput);
+const notes = document.getElementById('notes');
+// document.getElementsByName('difficulty').addEventListener('click',handleDiffChoice);
+const radioBtns = document.querySelectorAll('input[name="difficulty"]');
+document.getElementById('submit-bt').addEventListener('click', submitFn);
+document.addEventListener('keydown', function (e) {
+    if (e.key == 'Enter') {
+        submitFn();
+    }
+});
+updateUI();
+
+radioBtns[0].checked = true;
+
+function handleInput(event) {
+    typed_content = event.target.value;
+}
+function submitFn() {
+    if (typed_content == '') {
+        return;
+    }
+    for (const radioBtn of radioBtns) {
+        if (radioBtn.checked) {
+            switch (radioBtn.value) {
+                case "Easy":
+                    chosen_difficulty = "*";
+                    break;
+                case "Medium":
+                    chosen_difficulty = "**";
+                    break;
+                case "Hard":
+                    chosen_difficulty = "***";
+                    break;
+            }
+        }
+    }
+    savedNotes.push({ content: typed_content, difficulty: chosen_difficulty, date: current_date });
+    localStorage.setItem('savedNotes', JSON.stringify(savedNotes));
+    updateUI();
+    textInputElem.value = '';
+    typed_content = '';
+
+}
+
+function updateUI() {
+    notes.innerHTML = '';
+    savedNotes.forEach(element => {
+        notes.innerHTML += `<div class="note-card">
+        <div class="note-card-content">${element.content}</div>
+        <div class="difficulty">${element.difficulty}</div>
+        <div class="date">${element.date}</div>
+        </div>`;
+    });
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Things-I-Did-Tracker
 Momentum building app, designed to fight impostor syndrome.
+
+## Features
+* dynamic web dev using `js`
+* presistant browser local storage 
+* notes saved with the current date and with difficulty ranking system
+### Coming soon
+* animations
+* design overhaul
+* delete note function
+
+## Who is it for
+> people wanting to build real confidence in themselves and combat being burned out
+
+## How to run
+> Visit the [page](https://aminreo.github.io/Things-I-Did-Tracker/) for repo to use the live demo
+
+## Example usage
+ >  Simply run it in the morning to get energized or at night to reflect on things done during the day
+
+## Why chose this
+> Easy way to constanty build confidence in self by reflecting on things one did, no matter how small, it's all steps towards something greater
